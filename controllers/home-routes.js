fa0daa94ec75b52be5c6f005ef31a8dfbdccbefe
noplur <geoduckphoto@gmail.com>
2021-03-19 22:45:54 -0700

    added axios

---
+++ controllers/home-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User } = require('../models');
+
+// get all posts for homepage
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+    router.get('/signup', (req, res) => {
+      if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+      }
+      
+      res.render('signup');
+    });
+    
+    module.exports = router;
