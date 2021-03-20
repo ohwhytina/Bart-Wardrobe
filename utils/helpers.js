f7a5fccd7f8996d88a0d66f8694707817ff8893d
noplur <geoduckphoto@gmail.com>
2021-03-19 23:08:51 -0700

    update to weather fetch

--- utils/helpers.js
+++ utils/helpers.js
@@ -9,47 +9,34 @@
 
   // function to get current weather for a city
 
-var getCityWeather = function(cityName) {
+  var weather = async(cityName) => {
 
-  // format the Open Weather api url
-  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey + "&units=imperial";
+    // format the Open Weather api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey + "&units=imperial";
 
-  // make a request to the url
-  
-  fetch(apiUrl)
-  .then(function(response) {
-  // request was successful
-  if (response.ok) {
-      response.json().then(function(data) {
-      displayCityWeather(data, cityName);
-      cityHistory (cityName);
-    });
+    // make a request to the url
+    
+    fetch(apiUrl)
+    .then(function(response) {
+    // request was successful
+    if (response.ok) {
+        response.json().then(function(data) {
+        displayCityWeather(data, cityName);
+        cityHistory (cityName);
+      });
 
-    // if invalid city name is typed into search bar
-    } else {
-    alert("Error: " + response.statusText);
-    }
+      // if invalid city name is typed into search bar
+      } else {
+      alert("Error: " + response.statusText);
+      }
 
- })
-.then(function(response) {
-
-  // clears current-weather div class so elements will not repeat when another city name is enterred.
-
-  var currentWeatherContainerEl = document.querySelector('.current-weather');
-  currentWeatherContainerEl.innerHTML = '';
-
-})
-.catch(function(error) {
-  // alert if there is an error with fetch
-  alert("Unable to connect to Open Weather");
-});
+   })
 };
+
 
 
   // api calls
   
  var bartStation
 
- var weather
-
-  module.exports = { time, getCityWeather }
+  module.exports = { time, weather }
